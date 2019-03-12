2dbf9e8a6a5c3bbe7558650cd92f8a0ed0e40c64
Níkolas Vargas <vargasnikolass@gmail.com>
2019-03-12 00:05:26 -0300

    course model

--- simple_mooc/courses/models.py
+++ simple_mooc/courses/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField('courses name', max_length=30)
+    slug = models.SlugField('shortcut')
+    description = models.TextField('description', blank=True)
+    start_date = models.DateField('start date', null=True, blank=True)
+    image = models.ImageField(
+        upload_to='courses/images',
+        verbose_name='Image',
+        null=True,
+        blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField('last modified', auto_now=True)
