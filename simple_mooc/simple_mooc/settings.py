7f32b828fff0b03ad57c956b522b08df8324a898
Níkolas Vargas <vargasnikolass@gmail.com>
2019-03-12 00:05:16 -0300

    set new app and MEDIA_ROOT to media data

--- simple_mooc/simple_mooc/settings.py
+++ simple_mooc/simple_mooc/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'courses'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
