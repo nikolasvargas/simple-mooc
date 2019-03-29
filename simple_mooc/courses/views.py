b4def91124d02bc406f8806e96ff2d97bcd91a33
Níkolas Vargas <vargasnikolass@gmail.com>
2019-03-29 00:00:20 -0300

    render method more verbose, i think better

--- simple_mooc/courses/views.py
+++ simple_mooc/courses/views.py
@@ -6,10 +6,18 @@ from courses.models import Course
 def index(request):
     context = {}
     context['courses'] = Course.objects.all()
-    return render(request, 'index.html', context)
+    return render(
+        request=request,
+        template_name='index.html',
+        content=context
+        )
 
 
 def course(request, slug):
     context = {}
     context['course'] = Course.objects.get(slug=slug)
-    return render(request, 'course.html', context)
+    return render(
+        request=request,
+        template_name='course.html',
+        context=context
+    )
