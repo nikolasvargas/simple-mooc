1a49665060d4f2abe2d4c7ca34aabce834a13c4f
Níkolas Vargas <vargasnikolass@gmail.com>
2019-04-10 16:59:47 -0300

    generate fake data with Faker package

--- simple_mooc/courses/tests/test_models.py
+++ simple_mooc/courses/tests/test_models.py
@@ -1,9 +1,15 @@
 from django.test import TestCase
 from courses.models import Course
+from faker import Faker
 
+FAKE = Faker()
 
 class CourseModelTestCase(TestCase):
 
+    def setUp(self):
+        Course.objects.create(
+            name=FAKE.name()
+        )
+
     def test_create_course(self):
-        Course.objects.create(name='test')
         assert Course.objects.all().count() == 1
