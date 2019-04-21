040b1a62ba3be3de068d60a870a4bafa3da46770
Níkolas Vargas <vargasnikolass@gmail.com>
2019-04-21 00:09:01 -0300

    stuffs

--- simple_mooc/core/admin.py
+++ simple_mooc/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+import os
+import json
 
-# Register your models here.
+SETTING_FILE = open(os.path.dirname(__file__) + '/templates_config/template_teste.json').read()
+SITE_CONFIGURATION = json.loads(SETTING_FILE)
+
+admin.site.site_header = SITE_CONFIGURATION['SITE_NAME'] if SITE_CONFIGURATION['SITE_NAME'] else 'Admin Page'
--- simple_mooc/courses/views.py
+++ simple_mooc/courses/views.py
@@ -6,18 +6,10 @@ from courses.models import Course
 def index(request):
     context = {}
     context['courses'] = Course.objects.all()
-    return render(
-        request=request,
-        template_name='index.html',
-        context=context
-        )
+    return render(request, 'index.html', context)
 
 
 def course(request, slug):
     context = {}
     context['courses'] = Course.objects.get(slug=slug)
-    return render(
-        request=request,
-        template_name='course.html',
-        context=context
-    )
+    return render(request, 'course.html', context)
