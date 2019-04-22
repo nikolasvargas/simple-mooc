35828a734f9f9eb986242ceeaea39aa5be5a05bb
Níkolas Vargas <vargasnikolass@gmail.com>
2019-04-21 22:33:08 -0300

    setuptools now is more usefull

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@
 
 import sys
 import os
+import distutils.cmd
 from setuptools import find_packages, setup
 
 CURRENT_PYTHON_VERSION = sys.version_info[:2]
 REQUIRED_PYTHON_VERSION = (3, 5)
+REQUIREMENTS = []
 
 if CURRENT_PYTHON_VERSION < REQUIRED_PYTHON_VERSION:
     sys.stderr.write("""
@@ -19,19 +21,45 @@ This version of Django requires Python {}.{}, but you're trying to install it on
     sys.exit(1)
 
 
+class PrecommitCommand(distutils.cmd.Command):
+    """
+    Classe utilitária para instalar a ferramenta de pré commit
+    execute python setup.py pre_commit após rodar python setup.py install
+    """
+
+    user_options = []
+    description = 'Execute python setup.py pre_commit para instalar'
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('pre-commit install')
+
+
 def read(fname):
     with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        if os.path.splitext(fname)[-1] == '.txt':
+            return f.read().splitlines()
         return f.read()
 
 
 setup(
     name='Simple MOOC',
-    version='0.1dev',
+    version='1',
     python_requires='>={}.{}'.format(*REQUIRED_PYTHON_VERSION),
     author="Níkolas Vargas",
     author_email='vargasnikolass@gmail.com',
     license='MIT',
     packages=find_packages(),
-    install_requires=['Django', 'flake8', 'Pillow', 'faker'],
+    install_requires=[
+        read('requirements.txt')
+    ],
+    cmdclass={
+        'pre_commit': PrecommitCommand
+    },
     long_description=read('README.md')
 )
